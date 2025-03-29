57809c2e40568c0902aa05e9845ccd84f332b02c
Kazi Arman <153831689+kaziarman23@users.noreply.github.com>
2025-03-29 13:17:04 +0600

    added new get method

--- src/Controllers/User.Controllers.js
+++ src/Controllers/User.Controllers.js
@@ -1,18 +1,43 @@
 const db = require("../Configs/DB.Config");
 
-const GetAllUsers = async (req, res) => {};
-const GetOneUser = async (req, res) => {};
+const GetAllUsers = async (req, res) => {
+  try {
+    const [result] = await db.query("SELECT * FROM users");
+    console.log(result);
+    res.status(200).json({
+      message: "Viewing all the users",
+      result: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const GetOneUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [result] = await db.query(
+      `SELECT * FROM users where id= ? `,
+      [id]
+    );
+    console.log(result);
+    res.status(200).json({
+      message: "user details",
+      result: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 const CreateUser = async (req, res) => {
   const { name, email, password } = req.body;
+
   try {
     const [result] = await db.query(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
       [name, email, password]
     );
-    const data = await db;
-    console.log("database: ", data);
-    console.log("result: ", result);
-
     res.status(201).json({
       message: "User created",
       userId: result.insertId,
@@ -21,8 +46,11 @@ const CreateUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
 const UpdateUser = async (req, res) => {};
+
 const DeleteUser = async (req, res) => {};
+
 const DeleteAllUser = async (req, res) => {};
 
 module.exports = {
