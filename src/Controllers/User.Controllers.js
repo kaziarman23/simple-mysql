5a660814c68faf66ebbea40503f9ab92751d8378
Kazi Arman <153831689+kaziarman23@users.noreply.github.com>
2025-04-02 15:17:18 +0600

    added full crud oparetions

--- src/Controllers/User.Controllers.js
+++ src/Controllers/User.Controllers.js
@@ -47,11 +47,65 @@ const CreateUser = async (req, res) => {
   }
 };
 
-const UpdateUser = async (req, res) => {};
+const UpdateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
 
-const DeleteUser = async (req, res) => {};
+    // finding the existing user
+    const [existingUser] = await db.query(
+      "SELECT * FROM users where id = ?",
+      [id]
+    );
 
-const DeleteAllUser = async (req, res) => {};
+    const updatedName = name || existingUser.name;
+    const updatedEmail = email || existingUser.email;
+    const updatedPassword =
+      password || existingUser.password;
+
+    const updatedResult = await db.query(
+      "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ? ",
+      [updatedName, updatedEmail, updatedPassword, id]
+    );
+
+    res.status(201).json({
+      message: "User Updated successfully",
+      UpdatedUser: { id, name, email, password },
+      UpdatedUserDetails: updatedResult,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const DeleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [result] = await db.query(
+      "DELETE FROM  users where id = ?",
+      [id]
+    );
+    res.status(201).json({
+      message: "User Deleted Successfully",
+      result: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const DeleteAllUser = async (req, res) => {
+  try {
+    const [result] = await db.query("DELETE FROM users");
+    res.status(201).json({
+      message: "All users deleted",
+      result: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   GetAllUsers,
